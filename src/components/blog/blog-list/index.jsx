a5dcf4201983328e4e507d62babd400dda0fe94e
iCarambaaa <dkpress1916@zoho.eu>
2021-10-27 09:31:25 +0200

    Update index.jsx

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
-import Loading from "../../loader";
+import Loader from "../../loader";
 
 export default class BlogList extends Component {
   state = {
@@ -32,7 +32,7 @@ export default class BlogList extends Component {
   render() {
     const { loading, error, blogs } = this.state;
     if (loading) {
-      return <Loading />;
+      return <Loader />;
     } else {
       if (error) {
         return <div>{error}</div>;
