a32a5233f6e7badfe08706755c94fe73592c2a11
iCarambaaa <dkpress1916@zoho.eu>
2021-10-27 09:27:00 +0200

    Update index.jsx

--- src/views/new/index.jsx
+++ src/views/new/index.jsx
@@ -1,3 +1,109 @@
+// import React, { Component } from "react";
+// import "react-quill/dist/quill.snow.css";
+// import ReactQuill from "react-quill";
+// import { Container, Form, Button } from "react-bootstrap";
+// import "./styles.css";
+// export default class NewBlogPost extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { text: "", title: "", category: "" };
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+
+//   POSTNewBlogPost = async (e) => {
+//     e.preventDefault(e);
+//     try {
+//       const response = await fetch(
+//         "https://express-server-w1.herokuapp.com/posts",
+//         {
+//           method: "POST",
+//           body: JSON.stringify(this.state),
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//         }
+//       );
+//       if (response.ok) {
+//         console.log(response);
+//       } else {
+//         console.log(this.state);
+
+//         console.log(`wow... that wasn't supposed to happen... Error`);
+//         alert(`Woops we lost your data in the void .. try refreshing`);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+//   componentDidUpdate = async (e) => {};
+//   handleChange(value) {
+//     this.setState({ text: value, category: value, title: value });
+//   }
+
+//   render() {
+//     return (
+//       <Container className="new-blog-container">
+//         <Form className="mt-5">
+//           <Form.Group controlId="blog-form" className="mt-3">
+//             <Form.Label>Title</Form.Label>
+//             <Form.Control
+//               size="lg"
+//               placeholder="Title"
+//               type="text"
+//               onChange={(e) => this.handleChange(e)}
+//               value={this.state.title}
+//             />
+
+//             <div controlId="blog-category" className="mt-3">
+//               <Form.Label>Category</Form.Label>
+//               <Form.Control
+//                 size="lg"
+//                 as="select"
+//                 type="dropdown"
+//                 onChange={(e) => this.handleChange(e)}
+//                 value={this.state.category}
+//               >
+//                 <option>Blockchain</option>
+//                 <option>AI</option>
+//                 <option>Space</option>
+//                 <option>Literature</option>
+//                 <option>Biology</option>
+//               </Form.Control>
+//             </div>
+//             <div controlId="blog-content" className="mt-3">
+//               <Form.Label>Blog Content</Form.Label>
+//               <ReactQuill
+//                 value={this.state.text}
+//                 onChange={...this.state, this.handleChange}
+//                 className="new-blog-content"
+//               />
+//             </div>
+//             <div className="d-flex mt-3 justify-content-end">
+//               <Button
+//                 type="reset"
+//                 size="lg"
+//                 variant="outline-dark"
+//                 // onClick={this.setState({ text: "", title: "", category: "" })}
+//               >
+//                 Reset
+//               </Button>
+//               <Button
+//                 type="submit"
+//                 size="lg"
+//                 variant="dark"
+//                 style={{ marginLeft: "1em" }}
+//                 onClick={(e) => this.POSTNewBlogPost(e)}
+//               >
+//                 Submit
+//               </Button>
+//             </div>
+//           </Form.Group>
+//         </Form>
+//       </Container>
+//     );
+//   }
+// }
+
 import React, { Component } from "react";
 import "react-quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
@@ -6,38 +112,12 @@ import "./styles.css";
 export default class NewBlogPost extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: "", title: "", category: "" };
+    this.state = { text: "" };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  POSTNewBlogPost = async (e) => {
-    e.preventDefault(e);
-    try {
-      const response = await fetch(
-        "https://express-server-w1.herokuapp.com/posts",
-        {
-          method: "POST",
-          body: JSON.stringify(this.state),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      if (response.ok) {
-        console.log(response);
-      } else {
-        console.log(this.state);
-
-        console.log(`wow... that wasn't supposed to happen... Error`);
-        alert(`Woops we lost your data in the void .. try refreshing`);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  componentDidUpdate = async (e) => {};
   handleChange(value) {
-    this.setState({ text: value, category: value, title: value });
+    this.setState({ text: value });
   }
 
   render() {
@@ -46,57 +126,38 @@ export default class NewBlogPost extends Component {
         <Form className="mt-5">
           <Form.Group controlId="blog-form" className="mt-3">
             <Form.Label>Title</Form.Label>
-            <Form.Control
-              size="lg"
-              placeholder="Title"
-              type="text"
-              onChange={(e) => this.handleChange(e)}
-              value={this.state.title}
+            <Form.Control size="lg" placeholder="Title" />
+          </Form.Group>
+          <Form.Group controlId="blog-category" className="mt-3">
+            <Form.Label>Category</Form.Label>
+            <Form.Control size="lg" as="select">
+              <option>Category1</option>
+              <option>Category2</option>
+              <option>Category3</option>
+              <option>Category4</option>
+              <option>Category5</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="blog-content" className="mt-3">
+            <Form.Label>Blog Content</Form.Label>
+            <ReactQuill
+              value={this.state.text}
+              onChange={this.handleChange}
+              className="new-blog-content"
             />
-
-            <div controlId="blog-category" className="mt-3">
-              <Form.Label>Category</Form.Label>
-              <Form.Control
-                size="lg"
-                as="select"
-                type="dropdown"
-                onChange={(e) => this.handleChange(e)}
-                value={this.state.category}
-              >
-                <option>Blockchain</option>
-                <option>AI</option>
-                <option>Space</option>
-                <option>Literature</option>
-                <option>Biology</option>
-              </Form.Control>
-            </div>
-            <div controlId="blog-content" className="mt-3">
-              <Form.Label>Blog Content</Form.Label>
-              <ReactQuill
-                value={this.state.text}
-                onChange={this.handleChange}
-                className="new-blog-content"
-              />
-            </div>
-            <div className="d-flex mt-3 justify-content-end">
-              <Button
-                type="reset"
-                size="lg"
-                variant="outline-dark"
-                // onClick={this.setState({ text: "", title: "", category: "" })}
-              >
-                Reset
-              </Button>
-              <Button
-                type="submit"
-                size="lg"
-                variant="dark"
-                style={{ marginLeft: "1em" }}
-                onClick={(e) => this.POSTNewBlogPost(e)}
-              >
-                Submit
-              </Button>
-            </div>
+          </Form.Group>
+          <Form.Group className="d-flex mt-3 justify-content-end">
+            <Button type="reset" size="lg" variant="outline-dark">
+              Reset
+            </Button>
+            <Button
+              type="submit"
+              size="lg"
+              variant="dark"
+              style={{ marginLeft: "1em" }}
+            >
+              Submit
+            </Button>
           </Form.Group>
         </Form>
       </Container>
